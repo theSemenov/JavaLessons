46ee63dc261eec2023bc84da9b86bf1515bb286f
thesemenov <thesemenov@gmail.com>
2020-04-19 16:47:32 +0300

    s1

---
+++ src/ru/lesson1/part1/airport/AirportDto.java
@@ -0,0 +1,22 @@
+package ru.lesson1.part1.airport;
+
+public class AirportDto {
+    private String code;
+    private String name;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/ru/lesson1/part5/airport/EnemyAircraft.java
@@ -0,0 +1,26 @@
+package ru.lesson1.part5.airport;
+
+/**
+ * Вражеский самолет
+ */
+public class EnemyAircraft {
+
+    public AirportDto targetAirport;
+
+    private EnemyAircraft(AirportDto targetAirport) {
+        this.targetAirport = targetAirport;
+    }
+
+    /**
+     * Скинуть бомбу
+     */
+    public void throwBomb() {
+        if (targetAirport.getLatitude() == null) {
+            throw new IllegalArgumentException("Широта для аэропорта " + targetAirport.getName() + " не задана");
+        }
+        if (targetAirport.getLongitude() == null) {
+            throw new IllegalArgumentException("Долгота для аэропорта " + targetAirport.getName() + " не задана");
+        }
+        System.out.println("Пизда аэропорту " + targetAirport.getName().toLowerCase());
+    }
+}
